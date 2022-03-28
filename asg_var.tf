1f7844ffb8666932a325578df141f4ef01af647e
jhansi199 <99714685+jhansi199@users.noreply.github.com>
2022-03-28 11:57:14 +0530

    Update asg_var.tf

--- asg_var.tf
+++ asg_var.tf
@@ -1,6 +1,13 @@
-/*variable "availability_zones" {
-    default = "us-east-1b"
+variable "launch_configuration" {
+     default = "ALB Lconfigure"
+}
+variable "availability_zones" {
+    default = "us-east-1d"
+}
+variable "max_size" {
+    default = "5"
+}
+variable "min_size" {
+    default = "2"
 
 }
-*/
-
