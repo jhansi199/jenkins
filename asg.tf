c34f66ebd1be0ddbc8a2aaa559627d7f53adfac3
jhansi199 <99714685+jhansi199@users.noreply.github.com>
2022-03-28 11:56:28 +0530

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,4 +1,8 @@
-module "asg" {
-    source = "/home/ubuntu/terraform/folder2"
-}
+resource "aws_autoscaling_group" "ALBLconfigure" {
+       launch_configuration = var.launch_configuration
+         availability_zones = ["${var.availability_zones}"]
+         max_size           = var.max_size
+         min_size           = var.min_size
 
+
+}
