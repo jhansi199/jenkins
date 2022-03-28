430f622d23a23204148ec657948e9555df4cd029
jhansi199 <99714685+jhansi199@users.noreply.github.com>
2022-03-28 11:55:48 +0530

    Update lb_var.tf

--- lb_var.tf
+++ lb_var.tf
@@ -1,5 +1,15 @@
-/*module "lb" {
-source = "/home/ubuntu/terraform/folder2"
+variable "name" {
+   default = "test"
 }
-*/
-
+variable "load_balancer_type" {
+    default = "application"
+}
+variable "internal" {
+     default = "false"
+}
+variable "subnet_id1" {
+    default="subnet-02c2a08cacab1d4a1"
+}
+variable "subnet_id2" {
+    default="subnet-06785d1cd350e76d4"
+}
